9afd76799ff6420555040c8952216ec2cd08fed6
Arunjadhav0101 <arunj5687@gmail.com>
2025-04-01 03:53:55 +0000

    add py

---
+++ python/p6.py
@@ -0,0 +1,8 @@
+set1 ={1,2,3,4,5,6}
+set2 ={7,8,9,1,2,3}
+
+set_diff=set1-set2
+print("set differe in:: ",set_diff)
+
+symme_diff=set1^set2
+print("set symmetric is ::",symme_diff)
---
+++ python/p7.py
@@ -0,0 +1,7 @@
+set ={1,2,3,4,5,6,7,8,9}
+
+max_val=max(set)
+min_val=min(set)
+
+print("max vlaue",max_val)
+print("min value",min_val)
